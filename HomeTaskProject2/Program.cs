8a8b00cb6ad7b80a4787a40145060bdc5019f4d9
shahlarbh <shahlarbh@code.edu.az>
2022-12-30 18:24:43 +0400

    Add project files.

---
+++ HomeTaskProject2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace HomeTaskProject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1.
+            //Console.Write("Aylarin sayini daxil edin:");
+            //int month = int.Parse(Console.ReadLine());
+
+            //if (month >= 1 && month <= 1188) 
+            //{
+            //    int il = month / 12;
+            //    int ay = month % 12;
+
+            //    Console.WriteLine($"{month} ay = {il} il {ay} ay");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Intervala uygun ay duzgun daxil edilmeyib");
+            //}
+            #endregion
+
+            #region Task2.
+            //Console.Write("Ededi daxil edin:");
+            //int number = int.Parse(Console.ReadLine());
+            //int zeroCount = 0;
+            //int tmp = number;
+
+            //if (number > 9)
+            //{
+            //    while (tmp != 0)
+            //    {
+            //        if (tmp % 10 == 0)
+            //            zeroCount++;
+            //        tmp /= 10;
+            //    }
+            //    Console.WriteLine($"{number}-deki sifirlarin sayi = {zeroCount}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Daxil etdiyiniz eded 9-dan boyuk olmalidir");
+            //}
+            #endregion
+
+            #region Task3.
+            //Console.Write("Ededi daxil edin:");
+            //decimal number = decimal.Parse(Console.ReadLine());
+            //int real = (int)number;
+            //decimal kesr = number - real;
+            //decimal x = (real / 1000M);
+            //decimal y = kesr * 1000;
+            //decimal reverse = x + y;
+            //Console.WriteLine($"{reverse}");
+            #endregion
+
+        }
+    }
+}
